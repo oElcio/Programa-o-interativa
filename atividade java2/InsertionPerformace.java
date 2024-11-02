3e8819e43880e313f615879200e4d806d9cef25b
elcio <elcioferreira398@gmail.com>
2024-11-02 00:32:58 +0000

    começando em java

---
+++ atividade java2/InsertionPerformace.java
@@ -0,0 +1,13 @@
+// Exercicio 1.1
+public class InsertionPerformace {
+    public static void main(String[]args) {
+            int[] vetor ={34, 8, 64, 51, 32, 21, 4, 12, 67, 19, 78, 45, 34, 88, 29, 57, 83, 92, 6, 11};
+    
+            System.out.println("vetor original: ");
+            for(int i = 0; i< vetor.length; i++){
+                System.out.print(vetor[i] + " ");
+            } 
+        
+        }
+        
+    }
